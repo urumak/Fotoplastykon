3e0d17092c6730f1df50bde5d08ffefba072d9ed
Urszula Rumak <13crumak@gmail.com>
2020-02-05 21:21:11 +0100

    Poprawka na stronie głównej

--- fotoplastykon-web-public/src/views/Information/Home.vue
+++ fotoplastykon-web-public/src/views/Information/Home.vue
@@ -1,10 +1,10 @@
 <template>
     <div style="padding-left: 10%;padding-right: 10%;">
         <div v-for="(item, i) in Items" :key="item.id" class="row">
-            <v-card v-if="i % 2 !== 0" class="col-8 news-card">
-                <router-link :to="{ name: 'information-details', params: { id: item.id}}" class="card-title">
+            <v-card v-if="i % 2 !== 0" class="col-8 news-card" :to="{ name: 'information-details', params: { id: item.id}}">
+                <div class="card-title">
                     {{ item.title }}
-                </router-link>
+                </div>
                 <div class="card-title-line"></div>
                 <div class="news-card-content">
                     {{ item.introduction }}
@@ -13,10 +13,10 @@
             <v-img v-if="i % 2 !== 0" class="news-img" :src="item.photoUrl" contain>
             </v-img>
             <v-img v-if="i % 2 === 0" class="news-img-reflection" :src="item.photoUrl" contain></v-img>
-            <v-card v-if="i % 2 === 0" class="col-8 news-card">
-                <router-link :to="{ name: 'information-details', params: { id: item.id}}" class="card-title">
+            <v-card v-if="i % 2 === 0" class="col-8 news-card" :to="{ name: 'information-details', params: { id: item.id}}">
+                <div class="card-title">
                     {{ item.title }}
-                </router-link>
+                </div>
                 <div class="card-title-line"></div>
                 <div class="news-card-content-reflection">
                     {{ item.introduction }}
